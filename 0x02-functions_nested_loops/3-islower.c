32ed0750a626459126bfaae08589539a96173f9a
Ian Powell <ndaloianpowell@gmail.com>
2023-06-20 13:38:02 -0700

    lower case characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,15 @@
  * @c: The character in ASCII Table
  * Return: 1 if character is lowercase,0 otherwise
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
-
+	}
 	else
-	
+	{
 		return (0);
-	
+	}
 	_putchar('\n');
 }
