ca9b47b34404625dc7e78e8fc34578bc4be5e100
Ian Powell <ndaloianpowell@gmail.com>
2023-06-20 14:01:02 -0700

    Absolutely an Integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,10 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @int: input number as an integer
+ * @n: input number as an integer
  * Return: Absolute value
  */
-int _abs(int)
+int _abs(int n)
 {
 	int n = 0;
 
