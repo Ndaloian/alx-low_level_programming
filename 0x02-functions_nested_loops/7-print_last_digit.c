a046b5a467d104c7ff51dbaa2a78262b9e139bd1
Ian Powell <ndaloianpowell@gmail.com>
2023-06-20 15:04:32 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,17 @@ int print_last_digit(int ld)
 {
 	int lastDigit;
 
-	if (ld < 0)
+	lastDigit = ld % 10;
+	if (lastDigit < 0)
 	{
-		lastDigit = -ld % 10;
+		-putchar(-lastDigit + 48);
+		return (-lastDigit);
 	}
 	else
 	{
-		lastDigit = ld % 10;
+		_putchar(lastDigit + 48);
+		return (lastDigit);
 	}
-	_putchar(lastDigit + '0');
 	_putchar('\n');
-	return (lastDigit);
 }
 
