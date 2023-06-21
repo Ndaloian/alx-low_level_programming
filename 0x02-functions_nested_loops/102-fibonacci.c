186a7dbb211091f647f6757a1395332147c69cf8
Ian Powell <ndaloianpowell@gmail.com>
2023-06-21 16:15:02 -0700

    Fibonacci first 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints the first 50 Fibonacci numbers
+ * starting with 1 and 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	printf("%d, %d", fib[0], fib[1]);
+
+	int i;
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		printf(", %d", fib[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
+
