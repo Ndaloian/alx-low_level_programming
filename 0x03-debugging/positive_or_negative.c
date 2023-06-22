5cb2f4be1eb9109f2b397378b9fee24409a0dfda
Ian Powell <ndaloianpowell@gmail.com>
2023-06-22 00:54:55 -0700

    Test projects

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Test a number if positive or negative
+ * Return: No value
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
