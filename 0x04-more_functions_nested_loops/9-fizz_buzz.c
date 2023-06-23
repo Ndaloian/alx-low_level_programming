f38f75c9387c837a2adcc6942e9dfe186b0869f6
Ian Powell <ndaloianpowell@gmail.com>
2023-06-23 03:57:20 -0700

    Debug

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-	long int n;
+	int n;
 
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (n % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf(" %ld", n);
+			printf("%d ", n);
 		}
 	}
 	printf("\n");
