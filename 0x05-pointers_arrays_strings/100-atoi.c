c000c1b981ad463853e075d5b6eb157db67af601
Ian Powell <ndaloianpowell@gmail.com>
2023-06-27 15:59:19 -0700

    Make changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,40 +8,20 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
-	int digit = s[i] - '0';
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
-	while (s[i] == ' ')
-		i++;
-
-	if (s[i] == '-')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-		sign = -1;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	else if (s[i] == '+')
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
+		res = (res * 10) + (s[i] - '0');
 		i++;
 	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > (INT_MAX - digit) / 10 && sign == 1)
-		{
-			return (INT_MAX);
-		}
-
-		if (result < (INT_MIN + digit) / 10 && sign == -1)
-		{
-			return (INT_MIN);
-		}
-
-		result = result * 10 + digit;
-		i++;
-	}
-
-	return (sign * result);
+	res *= sign;
+	return (res);
 }
-
