85793faf3727123809bdb3fdd9085eb0629e8d91
Ian Powell <ndaloianpowell@gmail.com>
2023-06-29 15:23:26 +0000

    Debug

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * *cap_string - capitalizes all words of a string
- * @c: character string
+ * @str: character string
  * Return: string c
  */
 char *cap_string(char *str)
@@ -12,19 +12,21 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (isspace(str[i]) || ispunct(str[i]))
+		if (isspace(str[i]) || str[i] == ',' || str[i] == ';'
+			    || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+			    str[i] == '"' || str[i] == '(' || str[i] == ')'
+			    || str[i] == '{' || str[i] == '}')
 		{
 			capitalize_next = 1;
 		}
 		else if (capitalize_next)
 		{
-			c[i] = toupper(str[i]);
+			str[i] = toupper(str[i]);
 			capitalize_next = 0;
 		}
+
 		i++;
 	}
 
-	return (c);
+	return (str);
 }
-
-
