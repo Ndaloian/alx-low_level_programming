ead4c78189e404c5727aa8cf8576ed5e48fb6f2d
Ian Powell <ndaloianpowell@gmail.com>
2023-07-04 00:25:07 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: character string
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+			_purchar(' ');
+		}
+
+		_purchar('\n');
+	}
+}
