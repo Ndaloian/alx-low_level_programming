eec97e43ef27cbb07d9d2151ada6f8f0a0090e9a
Ian Powell <ndaloianpowell@gmail.com>
2023-07-12 00:06:50 -0700

    Debug

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,7 +18,8 @@ int count_word(char *s)
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-			flag == 0;
+			flag = 0;
+
 		else if (flag == 0)
 		{
 			flag = 1;
@@ -43,7 +44,7 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-	words = count_words(str);
+	words = count_word(str);
 
 	if (words == 0)
 		return (NULL);
@@ -52,7 +53,7 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i < = len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
