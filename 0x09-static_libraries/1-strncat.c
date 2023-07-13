73d896c340e9be1b4cb26982ae189c03a024a29a
Ian Powell <ndaloianpowell@gmail.com>
2023-07-13 12:50:35 -0700

    Debug

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,30 @@
 #include "main.h"
-
 /**
- * *_strncat - concatenates two strings
- * @dest: string character
- * @src: string character
- * @n: Integer
- * Return: Pointer to the resulting string
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *temp = dest;
+	int i;
+	int j;
 
-	while (*temp)
-		temp++;
-
-	while (*src && n > 0)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		*temp = *src;
-		temp++;
-		src++;
-		n--;
+		i++;
 	}
-
-	*temp = '\0';
-
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
